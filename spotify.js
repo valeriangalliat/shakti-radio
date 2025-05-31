65e02d61710285a7a7b830b4b8562bb87f267a21
Val <val@codejam.info>
2025-05-31 09:39:20 -0700

    Update Spotify client

--- spotify.js
+++ spotify.js
@@ -4,7 +4,7 @@ const config = require('./config')
 const { state, saveState } = require('./state')
 const log = require('./log')
 
-const api = buddyList.wrapWebApi(new SpotifyWebApi({ spDcCookie: config.spotify.spDcCookie }))
+const api = new SpotifyWebApi()
 
 async function clearPlaylist (batch = 0) {
   const tracksResponse = await api.getPlaylistTracks(config.spotify.playlist)
@@ -19,16 +19,16 @@ async function clearPlaylist (batch = 0) {
 }
 
 async function getCurrentlyPlaying () {
-  const activity = await api.getFriendActivity()
-  return activity.body.friends.find(friend => friend.user.name === config.spotify.friendName)
+  const activity = await buddyList.getFriendActivity(state.spotifyToken.accessToken)
+  return activity.friends.find(friend => friend.user.name === config.spotify.friendName)
 }
 
 async function authenticateApi () {
   // No token or less than 1 minute left on token.
   if (!state.spotifyToken || state.spotifyToken.accessTokenExpirationTimestampMs < Date.now() + 1000 * 60) {
     log('Refreshing token')
-    const tokenResponse = await api.getWebAccessToken()
-    state.spotifyToken = tokenResponse.body
+    const tokenResponse = await buddyList.getWebAccessToken(config.spotify.spDcCookie)
+    state.spotifyToken = tokenResponse
     await saveState()
   }
 
